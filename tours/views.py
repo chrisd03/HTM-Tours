0520ee9b4c3e4afd0a6bea026fa9e5eb75bd9275
Chris Domingos <chrisd03@outlook.com>
2024-02-24 13:06:23 -0500

    rest api working

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
+class TouristViewSet(viewsets.ModelViewSet):
+    queryset = Tourist.objects.all()
+    serializer_class = TouristSerializer
 
+class TourGuideViewSet(viewsets.ModelViewSet):
+    queryset = Tour_Guide.objects.all()
+    serializer_class = TourGuideSerializer

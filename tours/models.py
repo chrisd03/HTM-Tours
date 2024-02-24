0769166a7b945e83c0c69a8108e18e38572c2f44
nadiavarga <fnr8wh@virginia.edu>
2024-02-24 11:40:00 -0500

    make tourist model

--- tours/models.py
+++ tours/models.py
@@ -9,3 +9,15 @@ class Tour_Guide(models.Model):
     country = models.CharField("Country", max_length=200)
     def __str__(self):
         return self.name
+
+
+class Tourist(models.Model):
+    name = models.CharField("Name", max_length=200)
+    age = models.IntegerField()
+    bio = models.TextField("Biography")
+    city = models.CharField("City", max_length=200)
+    state = models.CharField("State", max_length=200)
+    likes = models.JSONField(default=list)
+
+    def __str__(self):
+        return self.name
